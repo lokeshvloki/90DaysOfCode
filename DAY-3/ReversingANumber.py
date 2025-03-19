1b38a87ddfc3e9b87ed5f4942eeb20db291f39eb
lokeshvloki <lokesh2403@gmail.com>
2025-03-19 18:08:26 +0530

    Added DAY-3 directory with Python programs

---
+++ DAY-3/ReversingANumber.py
@@ -0,0 +1,2 @@
+num = int(input("Enter a number: "))
+print("Reversed Number:", int(str(num)[::-1]))
---
+++ DAY-3/SwappingWithoutTempVar.py
@@ -0,0 +1,3 @@
+a, b = map(int, input("Enter two numbers: ").split())
+a, b = b, a
+print(f"After swapping: a = {a}, b = {b}")
